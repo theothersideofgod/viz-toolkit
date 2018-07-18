ebf84781336749cbb568d6c7ae5aca594bb03e43
Liling Jiang <>
2018-07-18 15:04:51 +0800

    1. update checkbox and example

--- libs/components/checkbox/viz-checkbox.module.ts
+++ libs/components/checkbox/viz-checkbox.module.ts
@@ -4,9 +4,13 @@ import { CommonModule } from '@angular/common';
 import { VizCheckboxComponent } from './viz-checkbox.component';
 import { MatCheckboxModule } from '@angular/material';
 
-@NgModule({
-  imports: [CommonModule, MatCheckboxModule],
-  exports: [VizCheckboxComponent],
-  declarations: [VizCheckboxComponent]
-})
-export class VizCheckboxModule {}
+// @NgModule({
+//   imports: [CommonModule, MatCheckboxModule],
+//   exports: [VizCheckboxComponent],
+//   declarations: [VizCheckboxComponent]
+// })
+// export class VizCheckboxModule {}
+
+
+
+export const VizCheckboxModule = MatCheckboxModule
