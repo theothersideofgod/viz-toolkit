d2f4c3c59779fcf34d08ab1132d92a32792bc119
sugarball <sugarball@foxmail.com>
2018-07-27 17:44:16 +0800

    checkbox

--- src/assets/examples/checkbox-basic/checkbox-basic.component.ts
+++ src/assets/examples/checkbox-basic/checkbox-basic.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkbox-basic',
   templateUrl: './checkbox-basic.component.html',
-  styleUrls: ['./checkbox-basic.component.css']
+  styleUrls: ['./checkbox-basic.component.scss']
 })
 export class CheckboxBasicComponent implements OnInit {
 
