9dfd28bb8f6ef55a9b35f89bb9f73596b61df0d6
Paul <18552029298@163.com>
2018-07-30 18:00:07 +0800

    optimize slider-toggle docs

--- src/assets/examples/slide-toggle-basic/slide-toggle-basic.component.ts
+++ src/assets/examples/slide-toggle-basic/slide-toggle-basic.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'slide-toggle-basic',
-	templateUrl: 'slide-toggle-basic.component.html'
+  templateUrl: 'slide-toggle-basic.component.html',
+  styleUrls: ['slide-toggle-basic.component.scss']
 })
 
 export class SlideToggleBasicComponent implements OnInit {
