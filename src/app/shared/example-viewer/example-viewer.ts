deba1a14a0ed2becab15f1300616dfaba6f74ceb
Liling Jiang <>
2018-08-07 13:59:09 +0800

    1. Abundan iframe viewport technique

--- src/app/shared/example-viewer/example-viewer.ts
+++ src/app/shared/example-viewer/example-viewer.ts
@@ -63,67 +63,9 @@ export class ExampleViewer {
   }
 
   ngOnInit() {
-    // this.iframe.nativeElement.src = "http://a.html"
   }
-
-  
-  insertComponentToIframe() {
-
-    // this.iframe.nativeElement.appendChild()
-    let doc = this.iframe.nativeElement.contentDocument || 
-               this.iframe.nativeElement.contentWindow;
-
-    let header = document.getElementsByTagName('head')[0].cloneNode(true)
-
-
-    doc.getElementsByTagName('head')[0].remove()
-    doc.getElementsByTagName('html')[0].prepend(header)
-
-
-    
-
-    // iframeHeader.appendChild(document.getElementsByTagName('head')[0])
-
-    doc.body.innerHTML = ""       
-
-    setTimeout(()=>{
-      const compFactory = this.resolver.resolveComponentFactory(this.exampleData.component);
-      this.compRef = this.vcRef.createComponent(compFactory);
-      doc.body.appendChild(this.compRef.location.nativeElement);
-
-
-
-      if(this._isScroll(this.exampleViewBody)){
-
-        console.log(this.iframe.nativeElement.clientWidth)
-        console.log(this.exampleViewBody.nativeElement.clientWidth)
-  
-        let ratio = (this.exampleViewBody.nativeElement.clientWidth) / 1200
-  
-        this.iframe.nativeElement.style.transform = `scale(${ratio})`
-  
-        console.log(this.exampleViewBody.nativeElement.clientWidth - this.iframe.nativeElement.clientWidth)
-  
-        let scrollCenter = (this.exampleViewBody.nativeElement.scrollWidth - this.exampleViewBody.nativeElement.clientWidth) / 2
-  
-        this.exampleViewBody.nativeElement.scrollLeft = scrollCenter
-      
-  
-      
-        
-      }
-
-
-
-    },0)
-
-
-   
-
-  }
-
-
-  _isScroll(e:ElementRef) {
-    return e.nativeElement.clientWidth < e.nativeElement.scrollWidth;
+ 
+  getViewportClass() {
+    return `${this._example} ${this.platform}`
   }
 }
