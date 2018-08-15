cb706459cf605a261fd8b11bef2c04cec3822a04
Liling Jiang <>
2018-08-15 15:50:40 +0800

    Update Component : Toolbar Fix bug : Toolbar --> incorrect design

--- libs/components/toolbar/viz-toolbar.module.ts
+++ libs/components/toolbar/viz-toolbar.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from "@angular/core";
-import { VizToolbarComponent } from './viz-toolbar.component'
-import { CommonModule } from '@angular/common';
-import { MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
-import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from "@angular/material";
 
-@NgModule({
-  imports: [
-    CommonModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule
-  ],
-  exports: [
-    VizToolbarComponent
-  ],
-  declarations: [
-    VizToolbarComponent,
-  ],
-})
-export class VizToolbarMudule {}
+// import { NgModule } from "@angular/core";
+// import { VizToolbarComponent } from './viz-toolbar.component'
+// import { CommonModule } from '@angular/common';
+// import { MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+// import { FormsModule } from '@angular/forms';
+
+// @NgModule({
+//   imports: [
+//   import { VizToolbarMudule } from './viz-toolbar.module';
+//     MatIconModule,
+//     MatFormFieldModule,
+//     MatInputModule,
+//     FormsModule
+//   ],
+//   exports: [
+//     VizToolbarComponent
+//   ],
+//   declarations: [
+//     VizToolbarComponent,
+//   ],
+// })
+// export class VizToolbarMudule {}
+
+
+
+
+export const VizToolbarMudule = MatToolbarModule
