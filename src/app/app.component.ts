603b56f52c34915d1de1dac589a01bdf11437476
Liling Jiang <>
2018-09-07 14:09:18 +0800

    **8. Chore** Edit Footer >https://screencast.com/t/tHRzmyWQkWc9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   options = [
-    {
-      "id": 1,
-      "title": "Contact",
-      "link": "Contact"
-    },
-    {
-      "id": 2,
-      "title": "Vizient",
-      "link": "Vizient"
-    },
-    {
-      "id": 3,
-      "title": "Additional Brands",
-      "link": "Additional Brands"
-    }
+    // {
+    //   "id": 1,
+    //   "title": "Contact",
+    //   "link": "Contact"
+    // },
+    // {
+    //   "id": 2,
+    //   "title": "Vizient",
+    //   "link": "Vizient"
+    // },
+    // {
+    //   "id": 3,
+    //   "title": "Additional Brands",
+    //   "link": "Additional Brands"
+    // }
   ]
 }
