2d9b5ee2e02fff1501278cef893264dce31b5b31
Lucas Jiang <lucas@interactivelabs.com>
2018-09-30 01:30:43 +0800

    Fix Bug : fix viz-paginator export issue

--- libs/components/paginator/viz-paginator.module.ts
+++ libs/components/paginator/viz-paginator.module.ts
@@ -1,3 +1,10 @@
-import { MatPaginatorModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatPaginatorModule, MatTableModule } from '@angular/material';
 
-export const VizPaginatorModule = MatPaginatorModule;
+@NgModule({
+  imports: [CommonModule],
+  declarations: [],
+  exports: [MatPaginatorModule, MatTableModule]
+})
+export class VizPaginatorModule {}
