81ea342afbb374c2e16195aacbe47ca03e5c019e
Lucas Jiang <lucas@interactivelabs.com>
2018-09-30 02:53:51 +0800

    Fix Bug : Add util class exports

---
+++ libs/util/index.ts
@@ -0,0 +1,10 @@
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+  Sort
+} from '@angular/material';
+// dialog function
+export { MAT_DIALOG_DATA, MatDialog, MatDialogRef };
+// sort function
+export { Sort };
