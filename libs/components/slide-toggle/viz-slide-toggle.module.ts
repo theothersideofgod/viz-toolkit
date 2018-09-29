639647fede61cf8ef4302caa7416c1ddc5f7c8a7
Lucas Jiang <lucas@interactivelabs.com>
2018-09-29 21:57:04 +0800

    Fix bug : fix viz-slide-toggle export issue

--- libs/components/slide-toggle/viz-slide-toggle.module.ts
+++ libs/components/slide-toggle/viz-slide-toggle.module.ts
@@ -1,3 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material';
 
-export const VizSlideToggleModule = MatSlideToggleModule;
+@NgModule({
+  imports: [CommonModule],
+  declarations: [],
+  exports: [MatSlideToggleModule]
+})
+export class VizSlideToggleModule {}
