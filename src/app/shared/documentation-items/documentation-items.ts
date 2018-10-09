cc642b55dd3dbbc7b01f5243b707da8e14de618d
Lucas Jiang <lucas@interactivelabs.com>
2018-10-09 17:00:52 +0800

    Add documentation : okta

--- src/app/shared/documentation-items/documentation-items.ts
+++ src/app/shared/documentation-items/documentation-items.ts
@@ -21,6 +21,7 @@ const DOCS: {[key: string]: DocCategory[]} = {
       id: 'button&indicatiors',
       name: 'Button & Indicatiors',
       items: [
+        {id: 'okta', name: ''},
         {id: 'applicationtemplates', name: ''},
         {id: 'colors', name: ''},
         {id: 'typography', name: ''},
