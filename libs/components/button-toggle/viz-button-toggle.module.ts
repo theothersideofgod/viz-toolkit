f6fe0a7ba53bd1cb3eaef986d5b4f0a623585ab1
Lucas Jiang <lucas@interactivelabs.com>
2019-03-01 10:37:22 +0800

    Add VizButtonToggleModule

---
+++ libs/components/button-toggle/viz-button-toggle.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonToggleModule]
+})
+export class VizButtonToggleModule {}
