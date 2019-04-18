32b2d5cb3767f945c3dffa2a6d796d3872f614a6
Lucas Jiang <lucas@interactivelabs.com>
2019-04-18 15:20:05 +0800

    Update toolkit-v2 chip document

--- src/app/shared/sketch-link/sketch-link.ts
+++ src/app/shared/sketch-link/sketch-link.ts
@@ -58,7 +58,8 @@ const sketch_link = {
     'Insert / Toolkit Material Library / Components / Badge / Short or Long'
   ],
   chip: [
-    'Insert / Toolkit Material Library / Components / Chip / Input or Choice'
+    'Insert / Toolkit Material Library / Components / Chip / Choice',
+    'Insert / Toolkit Material Library / Components / Chip / Input'
   ],
   icon: [
     'Insert / Toolkit Material Library / ✱ / Icon / (Select desired icon)'
