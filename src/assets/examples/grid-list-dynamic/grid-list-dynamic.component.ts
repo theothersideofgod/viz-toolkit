52c06e769c111f41a735dec938084da654974b70
Lucas Jiang <lucas@interactivelabs.com>
2019-04-24 17:28:05 +0800

    grid-list fix bug change background color change text to number change guideline picture size

--- src/assets/examples/grid-list-dynamic/grid-list-dynamic.component.ts
+++ src/assets/examples/grid-list-dynamic/grid-list-dynamic.component.ts
@@ -17,9 +17,9 @@ export interface Tile {
 })
 export class GridListDynamicComponent {
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: '1', cols: 3, rows: 1, color: '#E9E9E9'},
+    {text: '2', cols: 1, rows: 2, color: '#E9E9E9'},
+    {text: '3', cols: 1, rows: 1, color: '#E9E9E9'},
+    {text: '4', cols: 2, rows: 1, color: '#E9E9E9'},
   ];
 }
